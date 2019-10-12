de06894d5120e23e49f4f793fc9cf2ea2119e5e0
lucas <loveloper44@gmail.com>
2019-10-12 15:47:51 +0900

    UserMapper추가
    도메인과 엔티티 사이의 변환을 위해 추가

---
+++ src/user/infra/persistence/mapper/UserMapper.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@nestjs/common';
+
+import { UserType, UserProvider } from '@user/constant';
+
+import { UserAssembler } from '@user/application/assembler/UserAssembler';
+import * as UserDto from '@user/application/assembler/dto/UserDto';
+
+import { User } from '@user/domain/model/User';
+
+import { UserEntity } from '@user/infra/persistence/entity/UserEntity';
+import { PasswordEntity } from '@user/infra/persistence/entity/PasswordEntity';
+
+@Injectable()
+export class UserMapper {
+  constructor(private userAssembler: UserAssembler) {}
+
+  toDomain(entity: UserEntity): User {
+    const { id, email, password, type, provider, version, createdAt } = entity;
+
+    let passwordDto: UserDto.Password | null = null;
+    if (password) {
+      passwordDto = {
+        hashedPassword: password.hashedPassword,
+        createdAt: password.createdAt,
+      };
+    }
+
+    let userDto: UserDto.User = {
+      id,
+      email,
+      password: passwordDto,
+      type: UserType[type],
+      provider: UserProvider[provider],
+      version,
+      createdAt,
+    };
+
+    return this.userAssembler.toDomain(userDto);
+  }
+
+  toEntity(domain: User): UserEntity {
+    const dto: UserDto.User = this.userAssembler.toDto(domain);
+    const { id, email, password, type, provider, version, createdAt } = dto;
+
+    let passwordEntity: PasswordEntity | null = null;
+
+    if (password) {
+      passwordEntity = new PasswordEntity();
+      passwordEntity.hashedPassword = password.hashedPassword;
+      passwordEntity.createdAt = password.createdAt;
+    }
+
+    const userEntity = new UserEntity();
+
+    userEntity.id = id;
+    userEntity.email = email;
+    userEntity.password = passwordEntity;
+    userEntity.type = type;
+    userEntity.provider = provider;
+    userEntity.createdAt = createdAt;
+    userEntity.version = version;
+
+    return userEntity;
+  }
+}
