e9ef7020dc42c0acd9d8cda63dd1dcd2bd2642e2
lucas <loveloper44@gmail.com>
2019-10-12 22:42:25 +0900

    Dispatch config, constant 추가

---
+++ src/dispatch/config/index.ts
@@ -0,0 +1,2 @@
+export const ACCESS_TOKEN_KEY = 'access token key';
+export const REFRESH_TOKEN_KEY = 'refresh token key';
---
+++ src/dispatch/constant/index.ts
@@ -0,0 +1,11 @@
+export enum DispatchStatus {
+  IDLE = 'IDLE',
+  WAITING = 'WAITING',
+  MATCHED = 'MATCHED',
+  CANCELED = 'CANCELED',
+}
+
+export enum UserType {
+  PASSENGER = 'PASSENGER',
+  DRIVER = 'DRIVER',
+}
